5273a3722881768a591d138b1b9ef59bce9606b2
Leandro Gomes <leandro.gomes@prodap.com.br>
2021-11-12 17:26:30 -0300

    feat: finish create main file to receive system args

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,13 @@
+import sys
+
+from inventory_report.inventory.inventory import Inventory
+
+
 def main():
-    pass
+    # check if have 2 arguments
+    if len(sys.argv) != 3:
+        # expected message in test...
+        raise Exception("Verifique os argumentos\n")
+
+    # passing args to class method
+    print(Inventory.import_data(*sys.argv[1:]))
