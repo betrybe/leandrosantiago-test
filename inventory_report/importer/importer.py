35814624113defaf8d1af34f4419f7e7ff2fab0c
Leandro Gomes <leandro.gomes@prodap.com.br>
2021-11-12 16:41:57 -0300

    refactor: adding ext checker to importer class

--- inventory_report/importer/importer.py
+++ inventory_report/importer/importer.py
@@ -1,5 +1,4 @@
 from abc import ABC, abstractmethod
-from os.path import exists
 from typing import List
 
 
@@ -7,6 +6,12 @@ class Importer(ABC):
     """
     Abstract importer class
     """
+
+    @classmethod
+    def _check_file_ext(cls, file_path: str, allowed_ext: str):
+        _, file_ext = file_path.lower().split(".", -1)
+        if file_ext != allowed_ext:
+            raise ValueError("Arquivo inválido")
 
     @abstractmethod
     def import_data(self, file_path: str) -> List[dict]:
