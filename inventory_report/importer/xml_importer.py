f5cceb4ba86d076e2b762bc2c9b21c876f5788b1
Leandro Gomes <leandro.gomes@prodap.com.br>
2021-11-12 16:48:40 -0300

    refactor: remove complexity and change import_data to class method (called in test...)

--- inventory_report/importer/xml_importer.py
+++ inventory_report/importer/xml_importer.py
@@ -9,28 +9,21 @@ class XmlImporter(Importer):
     Class to import xml files
     """
 
-    def import_data(self, file_path: str) -> List[dict]:  # noqa: C901
+    @classmethod
+    def import_data(cls, file_path: str) -> List[dict]:  # noqa: C901
         """
         Method to import xml file and parse
         """
-        try:
-            # read file and get dataset tag
-            element_dataset = list(ET.parse(file_path).getroot())
+        cls._check_file_ext(file_path, "xml")
 
-            # transform each record of list into a dict
-            content = []
-            for element_record in list(element_dataset):
-                # map each child element into a key with value of dict
-                dict_record = {}
-                for el_field in list(element_record):
-                    # when field is numeric parse this
-                    if el_field.text.isnumeric():
-                        dict_record[el_field.tag] = int(el_field.text)
-                    else:
-                        dict_record[el_field.tag] = el_field.text
+        # read file and get dataset tag
+        dataset = list(ET.parse(file_path).getroot())
 
-                content.append(dict_record)
-            return content
-        except Exception:
-            # when can't read file and return content throw specific error msg
-            raise TypeError(f"Cannot import XML file in path {file_path}")
+        # transform each record of list into a dict
+        items = []
+
+        for record in list(dataset):
+            # map each child element into a key with value of dict
+            items.append({el.tag: el.text for el in list(record)})
+
+        return items
