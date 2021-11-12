99f87e42dc6a6ba56e44a99af2ad5264d0f8c99c
Leandro Gomes <leandro.gomes@prodap.com.br>
2021-11-12 16:43:07 -0300

    refactor: remove complexity and change import_data to class method (called in test...)

--- inventory_report/importer/csv_importer.py
+++ inventory_report/importer/csv_importer.py
@@ -9,16 +9,15 @@ class CsvImporter(Importer):
     Class to import csv file and parse
     """
 
-    def import_data(self, file_path: str) -> List[dict]:
+    @classmethod
+    def import_data(cls, file_path: str) -> List[dict]:
         """
         Method to import csv file and parse
         """
-        try:
-            # read file in path
-            with open(file_path) as csv_file:
-                # read and return content formated
-                content = csv.DictReader(csv_file)
-                return list(content)
-        except Exception:
-            # when can't read file and return content throw specific error msg
-            raise TypeError(f"Cannot import CSV file in path {file_path}")
+        cls._check_file_ext(file_path, "csv")
+
+        # read file in path
+        with open(file_path) as csv_file:
+            # read and return content formated
+            content = csv.DictReader(csv_file)
+            return list(content)
