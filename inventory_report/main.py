e2a662c7d07eaf1af9c0040a9d7a793ce99d73b5
Leandro Gomes <leandro.gomes@prodap.com.br>
2021-11-24 14:41:57 -0300

    refactor: implement bonus stage

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,6 +1,9 @@
 import sys
 
-from inventory_report.inventory.inventory import Inventory
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
 
 
 def main():
@@ -9,5 +12,19 @@ def main():
         # expected message in test...
         raise Exception("Verifique os argumentos\n")
 
-    # passing args to class method
-    print(Inventory.import_data(*sys.argv[1:]))
+    _, file_path, report_type = sys.argv
+
+    # map importers with extensions
+    importers = {"csv": CsvImporter, "json": JsonImporter, "xml": XmlImporter}
+
+    # get file extension
+    _, file_ext = file_path.lower().split(".", -1)
+
+    # create instance of inventory refactor
+    inventory = InventoryRefactor(importers[file_ext])
+
+    # generate report
+    result = inventory.import_data(file_path, report_type)
+
+    # and print
+    print(result)
